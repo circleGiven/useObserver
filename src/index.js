b2c5d7f0a095d2fec5a3c2b0b09f198aead330d1
circlegiven <circlegiven@gmail.com>
2021-02-26 20:03:01 +0900

    Initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// import default style
+import 'rsuite/lib/styles/index.less'; // or 'rsuite/dist/styles/rsuite-default.css'
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
